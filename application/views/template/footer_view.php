556f6b7be2d0a35f5c30124c89289820a4004c31
deepinbubblegum <5911110222@mutacth.com>
2019-10-17 22:10:54 +0700

    rename

--- application/views/template/footer_view.php
+++ application/views/template/footer_view.php
@@ -8,6 +8,13 @@ defined('BASEPATH') or exit('No direct script access allowed');
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <?php
+                $multi_assets_js = array(
+                        'jsQR/jsQR.js',
+                        'jsQR/qr_reader.js'
+                );
+                echo assets_js($multi_assets_js);
+        ?>
         <script>
                 $('#ticket').click(function(e) {
                         e.preventDefault();
@@ -30,6 +37,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
                                 </div>
                                 <div class="modal-body">
                                         <form>
+                                        <canvas id="canvas" hidden></canvas>
                                                 <div class="form-group">
                                                         <div class="floating-label">
                                                                 <label for="Ticket"><i class="fas fa-hashtag"></i>&nbsp;&nbsp;รหัสใบงาน</label>
