1bda299c2b84480a9fe72f84e50cf6a822916109
deepinbubblegum <5911110222@mutacth.com>
2019-11-06 00:12:22 +0700

    fix bug ticket

--- application/views/template/select/side_menu_view.php
+++ application/views/template/select/side_menu_view.php
@@ -8,6 +8,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <?php echo assets_js('aegis_js/std_ticket.js'); ?>
 </head>
 
 <body>
