189a378be4bd23be1ed9612c05f0efb8de38c821
deepinbubblegum <5911110222@mutacth.com>
2019-11-19 19:59:39 +0700

    remove tmp

--- application/views/teacher/uploads_view.php
+++ application/views/teacher/uploads_view.php
@@ -46,14 +46,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
                                         &nbsp; DOWNLOADS
                                 </span>
                         </div>
-                        <!-- <form class="form-inline">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn btn-secondary my-2 my-sm-0" type="submit">
-                                <span style="font-size: 1.1em;">
-                                        <i class="fas fa-search"></i></span>
-                                &nbsp;ค้นหา
-                        </button>
-                </form> -->
                         <button class="btn btn btn-secondary my-2 my-sm-0" data-toggle="modal" data-target="#addMenuupload">
                                 <span style="font-size: 1.1em;">
                                         <i class="fas fa-plus"></i></span>
@@ -129,48 +121,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
                                 <div aria-labelledby="profile-tab" class="tab-pane fade" id="profile" role="tabpanel">
                                         <div aria-labelledby="home-tab" class="tab-pane fade show active" id="home" role="tabpanel">
                                                 <div class="list-group mt-3 showUploaded" id="accordionTwo">
-                                                        <div class="expansion-panel list-group-item show">
-                                                                <a aria-controls="collapseOne" aria-expanded="true" class="expansion-panel-toggler collapsed" data-toggle="collapse" href="#collapseOne" id="headingOne">
-                                                                        Expansion panel item #1
-                                                                        <div class="expansion-panel-icon ml-3 text-black-secondary">
-                                                                                <i class="collapsed-show material-icons">keyboard_arrow_down</i>
-                                                                                <i class="collapsed-hide material-icons">keyboard_arrow_up</i>
-                                                                        </div>
-                                                                </a>
-                                                                <div aria-labelledby="headingOne" class="collapse show" data-parent="#accordionOne" id="collapseOne">
-                                                                        <div class="expansion-panel-body">
-                                                                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                                                                        </div>
-                                                                </div>
-                                                        </div>
-                                                        <div class="expansion-panel list-group-item">
-                                                                <a aria-controls="collapseTwo" aria-expanded="false" class="expansion-panel-toggler collapsed" data-toggle="collapse" href="#collapseTwo" id="headingTwo">
-                                                                        Expansion panel item #2
-                                                                        <div class="expansion-panel-icon ml-3 text-black-secondary">
-                                                                                <i class="collapsed-show material-icons">keyboard_arrow_down</i>
-                                                                                <i class="collapsed-hide material-icons">keyboard_arrow_up</i>
-                                                                        </div>
-                                                                </a>
-                                                                <div aria-labelledby="headingTwo" class="collapse" data-parent="#accordionOne" id="collapseTwo">
-                                                                        <div class="expansion-panel-body">
-                                                                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                                                                        </div>
-                                                                </div>
-                                                        </div>
-                                                        <div class="expansion-panel list-group-item">
-                                                                <a aria-controls="collapseThree" aria-expanded="false" class="expansion-panel-toggler collapsed" data-toggle="collapse" href="#collapseThree" id="headingThree">
-                                                                        Expansion panel item #3
-                                                                        <div class="expansion-panel-icon ml-3 text-black-secondary">
-                                                                                <i class="collapsed-show material-icons">keyboard_arrow_down</i>
-                                                                                <i class="collapsed-hide material-icons">keyboard_arrow_up</i>
-                                                                        </div>
-                                                                </a>
-                                                                <div aria-labelledby="headingThree" class="collapse" data-parent="#accordionOne" id="collapseThree">
-                                                                        <div class="expansion-panel-body">
-                                                                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                                                                        </div>
-                                                                </div>
-                                                        </div>
                                                 </div>
                                         </div>
                                 </div>
