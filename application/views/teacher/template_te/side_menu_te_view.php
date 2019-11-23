e6bf801d94e81b5e793cca4fabdf9c03eb505a17
deepinbubblegum <5911110222@mutacth.com>
2019-11-24 02:10:39 +0700

    add cropper

--- application/views/teacher/template_te/side_menu_te_view.php
+++ application/views/teacher/template_te/side_menu_te_view.php
@@ -8,6 +8,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <?php
+                echo assets_js('');
+        ?>
 </head>
 
 <body>
@@ -21,7 +24,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
                                                                 <span style="font-size: 1.5em;">
                                                                         <i class="far fa-address-card"></i></span>
                                                                 <span style="font-size: 1.2em;">
-                                                                        &nbsp;&nbsp;เพิ่มวิชาสอนประจำเทอม      
+                                                                        &nbsp;&nbsp;เพิ่มวิชาสอนประจำเทอม
                                                                 </span>
                                                         </a>
                                                         <a class="nav-item nav-link" href="<?php echo base_url('teacher'); ?>">
@@ -38,6 +41,15 @@ defined('BASEPATH') or exit('No direct script access allowed');
                                                                         &nbsp;&nbsp;นาฬิกาจับเวลา
                                                                 </span>
                                                         </a>
+
+                                                        <a class="nav-item nav-link" data-toggle="modal" data-target="#cropper_img">
+                                                                <span style="font-size: 1.5em;">
+                                                                        <i class="far fa-address-card"></i></span>
+                                                                <span style="font-size: 1.2em;">
+                                                                        &nbsp;&nbsp;
+                                                                </span>
+                                                        </a>
+
                                                         <div class="navdrawer-divider"></div>
                                                         <a href="">
                                                                 <p class="navdrawer-subheader"><i class="fas fa-exclamation-circle"></i>&nbsp;คู่มือใช้งานเว็บไซต์</p>
@@ -51,6 +63,43 @@ defined('BASEPATH') or exit('No direct script access allowed');
                                 </div>
                         </div>
 
+                        <div class="modal fade bd-example-modal-lg" id="cropper_img" tabindex="-1" role="dialog" data-backdrop="static" data-keyboard="false" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-lg">
+                                        <div class="modal-content">
+                                                <div class="modal-body">
+                                                        <div>
+                                                                <img id="image_preview" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/245f4571-14d4-4069-90a7-259b2971229f/dd8hkax-2446974f-4e52-4f85-8fb0-314ca6328ae8.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI0NWY0NTcxLTE0ZDQtNDA2OS05MGE3LTI1OWIyOTcxMjI5ZlwvZGQ4aGtheC0yNDQ2OTc0Zi00ZTUyLTRmODUtOGZiMC0zMTRjYTYzMjhhZTguanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.F2j_UIzKu9FXgQqRZOREorUYn-TBFGmfaebZCCFjqL8">
+                                                        </div>
+                                                </div>
+                                                <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary" data-target="#cropper_img" data-dismiss="modal">ปิด</button>
+                                                        <button type="button" class="btn btn-primary">บันทึก</button>
+                                                </div>
+                                        </div>
+                                </div>
+                        </div>
+
+                        <style>
+                                #image_preview {
+                                        max-width: 100%;
+                                }
+                        </style>
+
+                        <script>
+                                var $image = $('#image_preview');
+                                $image.cropper({
+                                        aspectRatio: 16 / 9,
+                                        crop: function(event) {
+                                                console.log(event.detail.x);
+                                                console.log(event.detail.y);
+                                                console.log(event.detail.width);
+                                                console.log(event.detail.height);
+                                                console.log(event.detail.rotate);
+                                                console.log(event.detail.scaleX);
+                                                console.log(event.detail.scaleY);
+                                        }
+                                });
+                        </script>
 </body>
 
 </html>
