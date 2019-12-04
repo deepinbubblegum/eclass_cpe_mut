ad17cc392cabde6623b30cc54bf0e9ba74287bb9
deepinbubblegum <5911110222@mutacth.com>
2019-12-05 02:47:39 +0700

    remove

---
+++ application/controllers/Te_media.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Te_media extends MY_Controller
+{
+        public function __construct()
+        {
+                parent::__construct();
+        }
+
+        private function upload_video()
+        {
+                
+        }
+
+}
--- public/assets/js/aegis_js/manage_te/te_media.js
+++ public/assets/js/aegis_js/manage_te/te_media.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-        
+        // ---------------function video-upload--------------
+        $('#video_file').change(function (e) { 
+                e.preventDefault();
+                html ='';
+                _files = $(this)[0].files;
+                console.log(_file[0].name);
+                $(this).next("label").text(_files[0].name);
+        });
 });
