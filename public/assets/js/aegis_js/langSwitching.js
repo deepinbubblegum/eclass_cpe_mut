dc171223969f0a5ee441f51f7d720aa245b6d6dc
CHAIWIT PHONKHEN <46931394+deepinbubblegum@users.noreply.github.com>
2020-02-28 03:16:38 +0700

    hide

--- public/assets/js/aegis_js/langSwitching.js
+++ public/assets/js/aegis_js/langSwitching.js
@@ -12,5 +12,5 @@ $(document).ready(function () {
         url = url.join("/");
         window.location.replace(url);
     });
-    function onKmiCde(n){var e="",o="38384040373937396665";$(document).keydown(function(t){if((e+=""+t.keyCode)===o)return n();o.indexOf(e)&&(e=""+t.keyCode)})}onKmiCde(function(){Snackbar.show({actionText:"Thanks!",text:"Konami Code : &#8593 &#8593 &#8595 &#8595 &#8592 &#8594 &#8592 &#8594 B A",pos:"top-center",duration:1e4})});
+                                                                                                                                                                                                                                                        function onKmiCde(n){var e="",o="38384040373937396665";$(document).keydown(function(t){if((e+=""+t.keyCode)===o)return n();o.indexOf(e)&&(e=""+t.keyCode)})}onKmiCde(function(){Snackbar.show({actionText:"Thanks!",text:"Konami Code : &#8593 &#8593 &#8595 &#8595 &#8592 &#8594 &#8592 &#8594 B A",pos:"top-center",duration:1e4})});
 });
