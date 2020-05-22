435d6365907036ba554265a1a76bfb65cd04e511
CHAIWIT PHONKHEN <46931394+deepinbubblegum@users.noreply.github.com>
2020-05-23 00:55:19 +0700

    update test

--- public/assets/js/jsQR/qr_reader.js
+++ public/assets/js/jsQR/qr_reader.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
+
+        function bootstrapClearButton() {
+                $('.position-relative :input').on('keydown focus', function () {
+                        if ($(this).val().length > 0) {
+                                $(this).nextAll('.form-clear').removeClass('d-none');
+                        }
+                }).on('keydown keyup blur', function () {
+                        if ($(this).val().length === 0) {
+                                $(this).nextAll('.form-clear').addClass('d-none');
+                        }
+                });
+                $('.form-clear').on('click', function () {
+                        $(this).addClass('d-none').prevAll(':input').val('');
+                });
+        }
+        bootstrapClearButton();
+
         // $('#qr_canvas').hide();
         // var video = document.createElement("video");
         // var canvasElement = document.getElementById("qr_canvas");
@@ -282,47 +299,202 @@ $(document).ready(function () {
         // });
 
 
+        // $('#qr_canvas').hide();
+        // video = document.getElementById('video');
 
-        video = document.getElementById('video');
-
-        navigator.mediaDevices.getUserMedia({
-                        video: {
-                                width: {
-                                        exact: 320
-                                },
-                                height: {
-                                        exact: 240
-                                }
-                        }
-                })
-                .then(function (stream) {
-                        if (navigator.mozGetUserMedia) {
-                                video.mozSrcObject = stream;
-                        } else {
-                                video.srcObject = stream;
-                        }
-                })
-                .catch(function (e) {
-                        alert(e);
-                        console.log(e)
-                });
+        // navigator.mediaDevices.getUserMedia({
+        //                 video: {
+        //                         width: {
+        //                                 exact: 320
+        //                         },
+        //                         height: {
+        //                                 exact: 240
+        //                         }
+        //                 }
+        //         })
+        //         .then(function (stream) {
+        //                 if (navigator.mozGetUserMedia) {
+        //                         video.mozSrcObject = stream;
+        //                 } else {
+        //                         video.srcObject = stream;
+        //                 }
+        //         })
+        //         .catch(function (e) {
+        //                 alert(e);
+        //                 console.log(e)
+        //         });
 
 
+        var video;
+        // var takePhotoButton;
+        var toggleFullScreenButton;
+        var switchCameraButton;
+        var amountOfCameras = 0;
+        var currentFacingMode = 'environment';
 
-        function bootstrapClearButton() {
-                $('.position-relative :input').on('keydown focus', function () {
-                        if ($(this).val().length > 0) {
-                                $(this).nextAll('.form-clear').removeClass('d-none');
-                        }
-                }).on('keydown keyup blur', function () {
-                        if ($(this).val().length === 0) {
-                                $(this).nextAll('.form-clear').addClass('d-none');
-                        }
-                });
-                $('.form-clear').on('click', function () {
-                        $(this).addClass('d-none').prevAll(':input').val('');
+        // this function counts the amount of video inputs
+        // it replaces DetectRTC that was previously implemented.
+        function deviceCount() {
+                return new Promise(function (resolve) {
+                        var videoInCount = 0;
+
+                        navigator.mediaDevices
+                                .enumerateDevices()
+                                .then(function (devices) {
+                                        devices.forEach(function (device) {
+                                                if (device.kind === 'video') {
+                                                        device.kind = 'videoinput';
+                                                }
+
+                                                if (device.kind === 'videoinput') {
+                                                        videoInCount++;
+                                                        console.log('videocam: ' + device.label);
+                                                }
+                                        });
+
+                                        resolve(videoInCount);
+                                })
+                                .catch(function (err) {
+                                        console.log(err.name + ': ' + err.message);
+                                        resolve(0);
+                                });
                 });
         }
-        bootstrapClearButton();
+
+        // check if mediaDevices is supported
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia && navigator.mediaDevices.enumerateDevices) {
+                // first we call getUserMedia to trigger permissions
+                // we need this before deviceCount, otherwise Safari doesn't return all the cameras
+                // we need to have the number in order to display the switch front/back button
+                navigator.mediaDevices
+                        .getUserMedia({
+                                audio: false,
+                                video: true,
+                        })
+                        .then(function (stream) {
+                                stream.getTracks().forEach(function (track) {
+                                        track.stop();
+                                });
+
+                                deviceCount().then(function (deviceCount) {
+                                        amountOfCameras = deviceCount;
+
+                                        // init the UI and the camera stream
+                                        initCameraUI();
+                                        initCameraStream();
+                                });
+                        })
+                        .catch(function (error) {
+                                //https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+                                if (error === 'PermissionDeniedError') {
+                                        alert('Permission denied. Please refresh and give permission.');
+                                }
+
+                                console.error('getUserMedia() error: ', error);
+                        });
+        } else {
+                alert('Mobile camera is not supported by browser, or there is no camera detected/connected');
+        }
+
+        function initCameraUI() {
+                video = document.getElementById('video');
+
+                toggleFullScreenButton = document.getElementById('toggleFullScreenButton');
+                switchCameraButton = document.getElementById('switchCameraButton');
+
+                // -- switch camera part
+                if (amountOfCameras > 1) {
+                        switchCameraButton.style.display = 'block';
+
+                        switchCameraButton.addEventListener('click', function () {
+                                if (currentFacingMode === 'environment') currentFacingMode = 'user';
+                                else currentFacingMode = 'environment';
+
+                                initCameraStream();
+                        });
+                }
+
+
+                window.addEventListener(
+                        'orientationchange',
+                        function () {
+                                // iOS doesn't have screen.orientation, so fallback to window.orientation.
+                                // screen.orientation will
+                                if (screen.orientation) angle = screen.orientation.angle;
+                                else angle = window.orientation;
+
+                                var guiControls = document.getElementById('gui_controls').classList;
+                                var vidContainer = document.getElementById('vid_container').classList;
+
+                                if (angle == 270 || angle == -90) {
+                                        guiControls.add('left');
+                                        vidContainer.add('left');
+                                } else {
+                                        if (guiControls.contains('left')) guiControls.remove('left');
+                                        if (vidContainer.contains('left')) vidContainer.remove('left');
+                                }
+
+                                //0   portrait-primary
+                                //180 portrait-secondary device is down under
+                                //90  landscape-primary  buttons at the right
+                                //270 landscape-secondary buttons at the left
+                        },
+                        false,
+                );
+        }
+
+        function initCameraStream() {
+                if (window.stream) {
+                        window.stream.getTracks().forEach(function (track) {
+                                console.log(track);
+                                track.stop();
+                        });
+                }
+
+                var size = 1280;
+
+                var constraints = {
+                        audio: false,
+                        video: {
+                                width: {
+                                        ideal: size
+                                },
+                                height: {
+                                        ideal: size
+                                },
+                                //width: { min: 1024, ideal: window.innerWidth, max: 1920 },
+                                //height: { min: 776, ideal: window.innerHeight, max: 1080 },
+                                facingMode: currentFacingMode,
+                        },
+                };
+
+                navigator.mediaDevices
+                        .getUserMedia(constraints)
+                        .then(handleSuccess)
+                        .catch(handleError);
+
+                function handleSuccess(stream) {
+                        window.stream = stream; // make stream available to browser console
+                        video.srcObject = stream;
+
+                        if (constraints.video.facingMode) {
+                                if (constraints.video.facingMode === 'environment') {
+                                        switchCameraButton.setAttribute('aria-pressed', true);
+                                } else {
+                                        switchCameraButton.setAttribute('aria-pressed', false);
+                                }
+                        }
+
+                        const track = window.stream.getVideoTracks()[0];
+                        const settings = track.getSettings();
+                        str = JSON.stringify(settings, null, 4);
+                        console.log('settings ' + str);
+                }
+
+                function handleError(error) {
+                        console.error('getUserMedia() error: ', error);
+                }
+        }
+
 
 });
